eda79e1450ffb5d04bc784e8033a021add9e0bf4
Arvind Raj T <46020497+Arvind-raj06@users.noreply.github.com>
2020-10-19 14:17:05 +0530

    Update kivysub2.py
    New version add on

--- kivysub2.py
+++ kivysub2.py
@@ -11,12 +11,17 @@ from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.config import Config
 from kivy.uix.dropdown  import DropDown
+from kivy.uix.spinner import Spinner
+from kivy.uix.screenmanager import (ScreenManager, Screen, NoTransition, 
+SlideTransition, CardTransition, SwapTransition, 
+FadeTransition, WipeTransition, FallOutTransition, RiseInTransition)
+from kivy.uix.progressbar import ProgressBar
 Config.set('graphics', 'window_state', 'maximized')
 typ="start"
 
 
 class FAWindow(Screen):
-    
+    global typ
     show="True"
     exp,st,text="","",""
     txt1 = ObjectProperty(None)
@@ -41,27 +46,31 @@ class FAWindow(Screen):
         popup = Popup(title ="Result", content = Label(text=self.show), 
                         size_hint =(None, None), size =(200, 200))
         popup.open()
-    global typ
+    def call(self, value):
+        if value=='Starts with':
+            typ="start"
+        elif value=='Ends with':
+            typ="end"
+        else:
+            typ="bet"
+        pass
+    
     def df(self):
-        sm.current="ndfa"
+        sm.current="dfa"
     def nf(self):
         sm.current="ndfa"
     
 class Ndfa(Screen):
-    def start(self):
-        typ="start"
+    def active(self):
         sm.current="comm"
-    def end(self):
-        typ="end"
-        sm.current="comm"
-    def ins(self):
-        typ="ins"
-        sm.current="comm"
+    pass
+class Dfa(Screen):
     def active(self):
         sm.current="comm"
     pass
 class CommonFA(Screen):
     show="True"
+    
     exp,st,text="","",""
     txt1 = ObjectProperty(None)
     txt2 = ObjectProperty(None)
@@ -72,28 +81,25 @@ class CommonFA(Screen):
         self.text = self.txt2.text 
         self.st=self.text
     def display(self):
-        self.show="True"
-        if typ=="start":
-            if not(self.st.startswith(self.exp)):
-                self.show="False"
-        elif typ=="end":
-            if not(self.st.endswith(self.exp)):
-                self.show="False"
-        else:
-            if self.exp not in self.st:
-                self.show="False"
+        #dfatrans(txt1,txt2,typ)
+        pb = ProgressBar(max=1000)
         popup = Popup(title ="Result", content = Label(text=self.show), 
                         size_hint =(None, None), size =(500, 500))
         popup.open()
+    def call(self, value):
+        typ=value
+        pass
+    def back(self):
+        sm.current="fai"
     pass
 
 class WindowManager(ScreenManager):
     pass
 
-sm = WindowManager()
+sm = WindowManager(transition=FadeTransition())
 kv = Builder.load_file("Auto2.kv")
 
-screens=[FAWindow(name="fai"), Ndfa(name="ndfa"), CommonFA(name="comm")]
+screens=[FAWindow(name="fai"), Ndfa(name="ndfa"), Dfa(name="dfa"),CommonFA(name="comm")]
 
 
 for screen in screens:
